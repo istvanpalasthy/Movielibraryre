2bebb9fe3577ba2228b78bb6896173ee7b022f3f
István Palásthy <istvanpalasthy@yahoo.com>
2019-12-03 11:03:52 +0100

    Add project files.

---
+++ Movielibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movielibrary
+{
+    class Movies
+    {
+         
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class FileHandler
+    {
+        public 
+    }
+}
