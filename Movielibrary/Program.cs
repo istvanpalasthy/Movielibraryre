24a490070f6053a00f3f04b796640c400ce3e39b
István Palásthy <istvanpalasthy@yahoo.com>
2019-12-03 11:49:53 +0100

    ittne

--- Movielibrary/Program.cs
+++ Movielibrary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,29 @@ namespace Movielibrary
 
     class FileHandler
     {
-        public 
+        public Dictionary<string, Dictionary<string, string>> IniConvertToDict(string filename)
+        {
+            Dictionary<string, Dictionary<string, string>> sectionContainer = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<string, string> properties = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(filename);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                if (line.Contains("[") && line.Contains("]"))
+                {
+                    properties = new Dictionary<string, string>();
+                    sectionContainer.Add(line, properties);
+                }
+                else if (line.Contains("="))
+                {
+                    string[] propertyList = line.Split("=");
+                    properties.Add(propertyList[0], propertyList[1]);
+                }
+                line = sr.ReadLine();
+            }
+            Console.WriteLine(sectionContainer);
+            return sectionContainer;
+        
+    }
     }
 }
